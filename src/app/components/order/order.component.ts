ade0e93d3617080b06ed29c6905163b71abe1084
21120455 <hoai0455fit@gmail.com>
2024-04-09 20:54:05 +0700

    update page order

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/app/environments/environment';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  cartItems: { product: Product, quantity: number }[] = [];
+  couponCode: string = '';
+  totalAmount: number = 0;
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService
+  ) { }
 
+  ngOnInit(): void {
+    // get list products
+    debugger
+    const cart = this.cartService.getCart();
+    const productIds = Array.from(cart.keys());    //convert to product contains only id
+    debugger
+    this.productService.getProductsByIds(productIds).subscribe({
+      next: (products) => {
+        debugger
+        this.cartItems = productIds.map(
+          (productId) => {
+            debugger
+            const product = products.find((p) => p.id === productId);
+            if (product) {
+              product.thumbnail = `${environment.apiBaseUrl}/products/images/${product.thumbnail}`;
+            }
+            return {
+              product: product!,
+              quantity: cart.get(productId)!
+            };
+          });
+        console.log('haha');
+      },
+      complete: () => {
+        debugger;
+        this.calculateTotal()
+      },
+      error: (error: any) => {
+        debugger;
+        console.error('Error fetching detail:', error);
+      }
+    });
+  }
+
+  //calculate total money
+  calculateTotal(): void {
+    this.totalAmount = this.cartItems.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    );
+  }
+
+  //Apply coupon when checkout
+  applyCoupon(): void {
+
+  }
 }
